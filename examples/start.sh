5b9af68d9e830504566a2fabb7c6c4c696502232
Tim Gross <tim@0x74696d.com>
2015-10-21 08:51:26 -0400

    Split up example into sub-apps

--- examples/start.sh
+++ examples/start.sh
@@ -31,19 +31,17 @@ else
     CONSUL_IP=${CONSUL_IP:-$(docker-machine ip default)}
 fi
 
-echo "Waiting for Consul at ${CONSUL_IP}"
+echo "Writing template values to Consul at ${CONSUL_IP}"
 while :
 do
-    # wait for consul to be live
+    # we'll sometimes get an HTTP500 here if consul hasn't completed
+    # it's leader election on boot yet, so poll till we get a good response.
     sleep 1
-    curl -s --fail -o /dev/null "http://${CONSUL_IP}:8500/v1/status/leader" && break
+    curl --fail -s -X PUT --data-binary @./nginx/default.ctmpl \
+         http://${CONSUL_IP}:8500/v1/kv/nginx/template && break
     echo -ne .
 done
-
-echo 'Writing template values to Consul.'
-curl -o /dev/null -X PUT --data-binary @./nginx/default.ctmpl \
-     http://${CONSUL_IP}:8500/v1/kv/nginx/template
-
+echo
 echo 'Opening consul console'
 open http://${CONSUL_IP}:8500/ui
 
@@ -57,16 +55,16 @@ else
     NGINX_IP=${NGINX_IP:-$(docker-machine ip default)}
 fi
 NGINX_PORT=$(docker inspect ${PREFIX}_nginx_1 | json -a NetworkSettings.Ports."80/tcp".0.HostPort)
-echo "Waiting for Nginx at ${NGINX_IP} to pick up initial configuration."
+echo "Waiting for Nginx at ${NGINX_IP}:${NGINX_PORT} to pick up initial configuration."
 while :
 do
     sleep 1
     curl -s --fail -o /dev/null "http://${NGINX_IP}:${NGINX_PORT}/app/" && break
     echo -ne .
 done
-
+echo
 echo 'Opening web page... the page will reload every 5 seconds with any updates.'
-open http://${NGINX_IP}:${NGINX_PORT}/app
+open http://${NGINX_IP}:${NGINX_PORT}/app/
 
 echo 'Try scaling up the app!'
 echo "${COMPOSE} scale app=3"
