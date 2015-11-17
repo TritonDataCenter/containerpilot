8a2f37d72fb09eb9a50666b3a59e8261f71871a0
Tim Gross <tim@0x74696d.com>
2015-11-17 10:45:42 -0500

    Improve test coverage of config parsing

--- src/containerbuddy/config_test.go
+++ src/containerbuddy/config_test.go
@@ -7,7 +7,9 @@ import (
 	"testing"
 )
 
-var testJson = `{
+func TestValidConfigParse(t *testing.T) {
+	defer argTestCleanup(argTestSetup())
+	var testJson = `{
     "consul": "consul:8500",
     "onStart": "/bin/to/onStart.sh",
     "services": [
@@ -41,24 +43,44 @@ var testJson = `{
 }
 `
 
-func TestConfigParse(t *testing.T) {
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
-	flag.Usage = nil
-	oldArgs := os.Args
-	defer func() { os.Args = oldArgs }()
-	os.Args = []string{"this", "-config", testJson, "/root/examples/test.sh", "doStuff", "--debug"}
+	os.Args = []string{"this", "-config", testJson, "/test.sh", "valid1", "--debug"}
 	config, _ := loadConfig()
 
 	if len(config.Backends) != 2 || len(config.Services) != 2 {
 		t.Errorf("Expected 2 backends and 2 services but got: %v", config)
 	}
 	args := flag.Args()
-	if len(args) != 3 || args[0] != "/root/examples/test.sh" {
+	if len(args) != 3 || args[0] != "/test.sh" {
 		t.Errorf("Expected 3 args but got unexpected results: %v", args)
 	}
 	if config.OnStart != "/bin/to/onStart.sh" {
 		t.Errorf("onStart not configured")
 	}
+}
+
+func TestInvalidConfigNoConfigFlag(t *testing.T) {
+	defer argTestCleanup(argTestSetup())
+	os.Args = []string{"this", "/test.sh", "invalid1", "--debug"}
+	if _, err := loadConfig(); err != nil && err.Error() != "-config flag is required." {
+		t.Errorf("Expected error but got %s", err)
+	}
+}
+
+func TestInvalidConfigParseNoDiscovery(t *testing.T) {
+	defer argTestCleanup(argTestSetup())
+	testParseExpectError(t, "{}", "No discovery backend defined")
+}
+
+func TestInvalidConfigParseFile(t *testing.T) {
+	defer argTestCleanup(argTestSetup())
+	testParseExpectError(t, "file:///xxxx",
+		"Could not read config file: open /xxxx: no such file or directory")
+}
+
+func TestInvalidConfigParseNotJson(t *testing.T) {
+	defer argTestCleanup(argTestSetup())
+	testParseExpectError(t, "<>",
+		"Could not parse configuration: invalid character '<' looking for beginning of value")
 }
 
 func TestGetIp(t *testing.T) {
@@ -94,5 +116,25 @@ func TestIpCheckPublic(t *testing.T) {
 		if !isPublicIp(ip) {
 			t.Errorf("Expected %s to be identified as public IP but got private.", ip)
 		}
+	}
+}
+
+// ----------------------------------------------------
+// test helpers
+
+func argTestSetup() []string {
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	flag.Usage = nil
+	return os.Args
+}
+
+func argTestCleanup(oldArgs []string) {
+	os.Args = oldArgs
+}
+
+func testParseExpectError(t *testing.T, testJson string, expected string) {
+	os.Args = []string{"this", "-config", testJson, "/test.sh", "test", "--debug"}
+	if _, err := loadConfig(); err != nil && err.Error() != expected {
+		t.Errorf("Expected %s but got %s", expected, err)
 	}
 }
