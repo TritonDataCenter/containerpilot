95687e471ad48b76c69f5bde0828033895c69cbf
Justen Walker <justen.walker+github@gmail.com>
2016-03-01 01:31:23 -0500

    Add logging config with logrus
    - Add LogConfig
    - Support go "log" package by default for backwards compatibility

---
+++ containerbuddy/logging.go
@@ -0,0 +1,92 @@
+package containerbuddy
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/Sirupsen/logrus"
+)
+
+// LogConfig configures the log levels
+type LogConfig struct {
+	Level  string `json:"level"`
+	Format string `json:"format,omitempty"`
+	Output string `json:"output,omitempty"`
+}
+
+var defaultLog = &LogConfig{
+	Level:  "INFO",
+	Format: "go",
+	Output: "stdout",
+}
+
+func init() {
+	if err := defaultLog.init(); err != nil {
+		log.Println(err)
+	}
+}
+
+func (l *LogConfig) init() error {
+	// Set defaults
+	if l.Level == "" {
+		l.Level = defaultLog.Level
+	}
+	if l.Format == "" {
+		l.Format = defaultLog.Format
+	}
+	if l.Output == "" {
+		l.Output = defaultLog.Output
+	}
+	level, err := logrus.ParseLevel(strings.ToLower(l.Level))
+	if err != nil {
+		return fmt.Errorf("Unknown log level '%s': %s", l.Level, err)
+	}
+	var formatter logrus.Formatter
+	var output io.Writer
+	switch strings.ToLower(l.Format) {
+	case "text":
+		formatter = &logrus.TextFormatter{}
+	case "json":
+		formatter = &logrus.JSONFormatter{}
+	case "go":
+		formatter = &GoLogFormatter{}
+	default:
+		return fmt.Errorf("Unknown log format '%s'", l.Format)
+	}
+	switch strings.ToLower(l.Output) {
+	case "stderr":
+		output = os.Stderr
+	case "stdout":
+		output = os.Stdout
+	default:
+		return fmt.Errorf("Unknown output type '%s'", l.Output)
+	}
+	log.Printf("=== SET FORMAT ===\n%#v\n", formatter)
+	logrus.SetLevel(level)
+	logrus.SetFormatter(formatter)
+	logrus.SetOutput(output)
+	return nil
+}
+
+// GoLogFormatter delegates formatting to standard go log package
+type GoLogFormatter struct {
+}
+
+// Format formats the logrus entry by passing it to the "log" package
+func (f *GoLogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	b := &bytes.Buffer{}
+	logger := log.New(b, "", log.LstdFlags)
+	switch entry.Level {
+	case logrus.PanicLevel:
+		logger.Panicln(entry.Message)
+	case logrus.FatalLevel:
+		logger.Fatalln(entry.Message)
+	default:
+		logger.Println(entry.Message)
+	}
+	return b.Bytes(), nil
+}
