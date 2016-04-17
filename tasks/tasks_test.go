0974b2a787c4b4abe63aee6083ba76019c4a014b
Justen Walker <justen.walker+github@gmail.com>
2016-04-17 16:51:25 -0400

    tasks: correct TestScheduledTaskTimeoutConfig

--- tasks/tasks_test.go
+++ tasks/tasks_test.go
@@ -1,6 +1,7 @@
 package tasks
 
 import "testing"
+import "runtime"
 import "time"
 import "io/ioutil"
 import "os"
@@ -51,7 +52,7 @@ func TestScheduledTaskTimeoutConfig(t *testing.T) {
 	}
 	task := &TaskConfig{
 		Args:      []string{"testdata/test.sh", "printDots", tmpf.Name()},
-		Frequency: "1000ms",
+		Frequency: "400ms",
 		Timeout:   "200ms",
 	}
 	err = parseTask(task)
@@ -61,7 +62,10 @@ func TestScheduledTaskTimeoutConfig(t *testing.T) {
 	// Should print 2 dots (timeout 250ms after printing 1 dot every 100ms)
 	expected := []byte("..")
 	task.Start()
-	ticker := time.NewTicker(1050 * time.Millisecond)
+	// Ensure the task has time to start
+	runtime.Gosched()
+	// Wait for task to start + 250ms
+	ticker := time.NewTicker(650 * time.Millisecond)
 	select {
 	case <-ticker.C:
 		task.Stop()
@@ -71,6 +75,6 @@ func TestScheduledTaskTimeoutConfig(t *testing.T) {
 		t.Fatalf("Unexpeced error: %v", err)
 	}
 	if !reflect.DeepEqual(expected, content) {
-		t.Errorf("Expected %v but got %v", expected, content)
+		t.Errorf("Expected %s but got %s", expected, content)
 	}
 }
