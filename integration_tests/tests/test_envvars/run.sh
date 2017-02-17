b1f1f52e6304b19e8add6f0dc85e475f6e033e28
Tim Gross <tim@0x74696d.com>
2017-02-17 16:00:57 -0500

    Simplify envvars test

--- integration_tests/tests/test_envvars/run.sh
+++ integration_tests/tests/test_envvars/run.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 set -e
 
-# stand up Consul
-docker-compose up -d consul
-
-# give the instance enough time to self-elect
-n=0
-while true
-do
-    if [ $n == 15 ]; then
-        echo 'Timed out waiting for Consul.'
-        exit 1;
-    fi
-    curl -Ls --fail "http://${DOCKER_HOST:-localhost}:8500/v1/status/leader" | grep 8300 >/dev/null 2>&1 && break
-    n=$((n+1))
-    sleep 1
-done
+# run and check that we didn't get the default environment value of "FAIL"
+docker-compose run --rm test | grep -v FAIL
