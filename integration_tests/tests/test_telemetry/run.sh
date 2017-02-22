8c1ecc020149bc426f695fe156e22d3a0a252e33
Tim Gross <tim@0x74696d.com>
2017-02-22 10:28:34 -0500

    Switch integration tests to Compose v2 syntax
    Under recent versions of Docker for Mac, DNS is not being correctly setup for
    containers with '--link', which means that queries for hosts will fall back to
    the underlying host when the golang resolver is in use (as it is by default
    with 'CGO_ENABLED=0'). The resolver will fall back to files and work fine if
    there's no response to the DNS query from the underlying host, but this leaks
    the DNS query to the Internet and will cause test failures if failing DNS
    queries are hijacked by the host's ISP. (Thanks Verizon FiOS!)

--- integration_tests/tests/test_telemetry/run.sh
+++ integration_tests/tests/test_telemetry/run.sh
@@ -19,13 +19,13 @@ if [ ! $? -eq 0 ] ; then exit 1 ; fi
 docker-compose up -d app
 
 APP_ID="$(docker-compose ps -q app)"
-IP=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' ${APP_ID})
+IP=$(docker inspect -f '{{ .NetworkSettings.Networks.testtelemetry_default.IPAddress }}' "${APP_ID}")
 
 # This interface takes a while to converge
 set +e
 for i in {20..1}; do
     sleep 1 # sleep has to be before b/c we want exit code
-    docker exec -it ${APP_ID} curl -s ${IP}:9090/metrics | grep 'containerpilot_app_some_counter 42' && break
+    docker exec -it "${APP_ID}" curl -s "${IP}:9090/metrics" | grep 'containerpilot_app_some_counter 42' && break
 done
 result=$?
 set -e
@@ -36,11 +36,11 @@ CONSUL_ID="$(docker-compose ps -q consul)"
 set +e
 for i in {5..1}; do
     sleep 1
-    docker exec -it ${CONSUL_ID} curl -s --fail localhost:8500/v1/catalog/service/containerpilot | grep 'containerpilot' && break
+    docker exec -it "${CONSUL_ID}" curl -s --fail localhost:8500/v1/catalog/service/containerpilot | grep 'containerpilot' && break
 done
 result=$?
 set -e
 if [ $result -ne 0 ]; then exit $result; fi
 
 docker-compose stop app
-docker exec -it ${CONSUL_ID} curl -s --fail localhost:8500/v1/catalog/service/containerpilot | grep -v 'containerpilot'
+docker exec -it "${CONSUL_ID}" curl -s --fail localhost:8500/v1/catalog/service/containerpilot | grep -v 'containerpilot'
