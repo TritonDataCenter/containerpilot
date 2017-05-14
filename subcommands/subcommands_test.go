9c4eb88051304bc03f771ea96c47810979319bf8
Justin Reagor <justinwr@gmail.com>
2017-05-14 18:29:50 +0000

    Make notes on subcommands tests

--- subcommands/subcommands_test.go
+++ subcommands/subcommands_test.go
@@ -6,16 +6,30 @@ import (
 
 func TestReloadCommand(t *testing.T) {
 	t.Skip("Not implemented")
+
+	// Stub out client...
+	// Nothing to test? Calls out to client...
 }
 
 func TestMaintenanceCommand(t *testing.T) {
 	t.Skip("Not implemented")
+
+	// Stub out client...
+	// Make sure sending bool parameter into client....
 }
 
 func TestEnvironCommand(t *testing.T) {
 	t.Skip("Not implemented")
+
+	// Stub out client
+	// Make sure map is sent to client as JSON
+	// Test variations of bad maps that break JSON
 }
 
 func TestMetricCommand(t *testing.T) {
 	t.Skip("Not implemented")
+
+	// Stub out client
+	// Make sure map is sent to client as JSON
+	// Test variations of bad maps that break JSON
 }
