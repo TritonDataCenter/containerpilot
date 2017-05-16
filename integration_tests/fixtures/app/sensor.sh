a836dc61b9f81cbfb707554b646d5459f4728aa9
Tim Gross <tim@0x74696d.com>
2017-05-16 11:12:22 -0400

    use -putmetric subcommand in telemetry integration test

--- integration_tests/fixtures/app/sensor.sh
+++ integration_tests/fixtures/app/sensor.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-echo -e 'POST /v3/metric HTTP/1.1\r\nHost: control\r\nContent-Type: application/json\r\nContent-Length: 39\r\n\r\n{"containerpilot_app_some_counter": 42}' | \
-    nc -U /var/run/containerpilot.socket
+/bin/containerpilot -putmetric 'containerpilot_app_some_counter=42'
