a1c4b60324ef216ab56552b882e39148a902f70e
Tim Gross <tim@0x74696d.com>
2017-05-17 09:30:46 -0400

    use -reload subcommand in config reload test (#362)

--- integration_tests/fixtures/app/reload-containerpilot.sh
+++ integration_tests/fixtures/app/reload-containerpilot.sh
@@ -2,18 +2,16 @@
 set -e
 
 single() {
-    echo -e "POST /v3/reload HTTP/1.1\r\nHost: control\r\n\r\n" | \
-        nc -U /var/run/containerpilot.socket > /dev/null 2>&1
+    /bin/containerpilot -reload > /dev/null 2>&1
     exit 0
 }
 
 multi() {
     # we mask the output here because we expect many many lines that
-    # say "nc: unix connect failed: No such file or directory" while
-    # the config is being reloaded
+    # say "dial unix /var/run/containerpilot.sock: connect: no such
+    # file or directory" while the config is being reloaded
     for i in {1..200}; do
-        echo -e "POST /v3/reload HTTP/1.1\r\nHost: control\r\n\r\n" | \
-            nc -U /var/run/containerpilot.socket > /dev/null 2>&1
+        /bin/containerpilot -reload > /dev/null 2>&1
     done
     exit 0
 }
