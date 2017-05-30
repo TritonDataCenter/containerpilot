1d14cc9f5978116855d2588d6dcf23708db149ae
Tim Gross <tim@0x74696d.com>
2017-05-30 11:43:16 -0400

    docs: name doc markdown output as docs.md

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -25,17 +25,17 @@ for file in $(find docs -name '*-*.md'); do
     f=${x%.md}
     buildDir="build/$dir/$f"
     mkdir -p "$buildDir"
-    cp "$file" "$buildDir/index.md"
-    addFrontMatter "$buildDir/index.md"
-    rewriteLinks "$buildDir/index.md"
+    cp "$file" "$buildDir/docs.md"
+    addFrontMatter "$buildDir/docs.md"
+    rewriteLinks "$buildDir/docs.md"
 done
 
 # top-level index is weird exception to the structure
 buildDir="build/docs/00-index"
 mkdir -p "$buildDir"
-cp docs/README.md "$buildDir/index.md"
-addFrontMatter "$buildDir/index.md"
-rewriteLinks "$buildDir/index.md"
+cp docs/README.md "$buildDir/docs.md"
+addFrontMatter "$buildDir/docs.md"
+rewriteLinks "$buildDir/docs.md"
 
 # configuration examples in JSON5 format
 cp -R docs/30-configuration/examples build/docs/30-configuration/examples
