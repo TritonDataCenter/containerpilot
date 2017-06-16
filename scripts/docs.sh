9275c7372ba815a9a39cad99c5173ad345a3065d
Tim Gross <tim@0x74696d.com>
2017-06-16 08:39:43 -0400

    remove accidental gnu-sed assumption in kirby doc gen (#404)

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -eu
 
 # rewrites the title as Kirby front-matter (in-place)
 addFrontMatter() {
-    title=$(head -1 "$1" | sed -r 's/^# //')
+    title=$(head -1 "$1" | sed 's/^# //')
     body=$(tail -n +2 "$1")
     {
         echo "Title: $title"
@@ -16,26 +17,27 @@ addFrontMatter() {
 
 # rewrites all markdown links to indexes (in-place)
 rewriteLinks() {
-    sed -i 's/\.md//g' "$1"
+    sed -i '.bak' 's/\.md//g' "$1"
+    rm "${1}.bak"
 }
 
 for file in $(find docs -name '*-*.md'); do
     x=$(basename "$file")
     dir=$(dirname "$file")
     f=${x%.md}
-    buildDir="build/$dir/$f"
+    buildDir="./build/${dir}/${f}"
     mkdir -p "$buildDir"
-    cp "$file" "$buildDir/docs.md"
-    addFrontMatter "$buildDir/docs.md"
-    rewriteLinks "$buildDir/docs.md"
+    cp "$file" "${buildDir}/docs.md"
+    addFrontMatter "${buildDir}/docs.md"
+    rewriteLinks "${buildDir}/docs.md"
 done
 
 # top-level index is weird exception to the structure
 buildDir="build/docs/00-index"
 mkdir -p "$buildDir"
-cp docs/README.md "$buildDir/docs.md"
-addFrontMatter "$buildDir/docs.md"
-rewriteLinks "$buildDir/docs.md"
+cp docs/README.md "${buildDir}/docs.md"
+addFrontMatter "${buildDir}/docs.md"
+rewriteLinks "${buildDir}/docs.md"
 
 # configuration examples in JSON5 format
 cp -R docs/30-configuration/examples build/docs/30-configuration/examples
