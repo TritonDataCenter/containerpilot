f4bc0885ab1c6550d6ac73004528f3dbf5f42c3f
Tim Gross <tim@0x74696d.com>
2017-06-19 10:00:59 -0400

    ensure zombies reaping test fails correctly

--- integration_tests/tests/test_reap_zombies/run.sh
+++ integration_tests/tests/test_reap_zombies/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 # Test to verify that we're correctly reaping zombies.
 # At any given time we may have up to 1 zombie parented to PID1 (it has been
 # reparented but not yet reaped) and 1 zombie not yet parented to PID1.
@@ -14,23 +15,19 @@ if [ ! $? -eq 0 ] ; then exit 1 ; fi
 APP_ID="$(docker-compose ps -q app)"
 sleep 6
 
-PTREE=$(docker exec $APP_ID ps -o stat,ppid,pid,comm)
-REPARENTED_ZOMBIES=$(echo $PTREE | awk '
-BEGIN { count=0 }
-$1 ~ /^Z/ && $2 ~ /^1$/ { count++ }
-END { print count }
-')
+PTREE=$(docker exec "$APP_ID" ps -o stat,ppid,pid,comm)
 
-TOTAL_ZOMBIES=$(echo $PTREE | awk '
-BEGIN { count=0 }
-$1 ~ /^Z/ { count++ }
-END { print count }
-')
+set +e
+REPARENTED_ZOMBIES=$(echo "$PTREE" | awk -F' +' '/^Z/{print $2}' | grep -c "^1$")
+TOTAL_ZOMBIES=$(echo "$PTREE" | grep -c "^Z")
+set -e
 
-if [ $REPARENTED_ZOMBIES -gt 1 ] || [ $TOTAL_ZOMBIES -gt 2 ]; then
-  echo "More than permitted number of zombies." >&2
-  echo $PTREE >&2
-  docker logs "${APP_ID}" | app.log
-  exit 1
+if [ "$REPARENTED_ZOMBIES" -gt 1 ] || [ "$TOTAL_ZOMBIES" -gt 2 ]; then
+    echo "More than permitted number of zombies." >&2
+    echo "- got $REPARENTED_ZOMBIES reparented zombies" >&2
+    echo "- got $TOTAL_ZOMBIES total zombies" >&2
+    echo "$PTREE" >&2
+    docker logs "${APP_ID}" > app.log
+    exit 1
 fi
 exit 0
