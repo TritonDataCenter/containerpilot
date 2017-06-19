9f715fb45835e188ff45ca15500ce949d5c9f019
Tim Gross <tim@0x74696d.com>
2017-06-19 10:58:06 -0400

    send reapChildren the pid rather the pgid when the pgid is 0.
    Whenever our pgid is 0, the reapChildren function won't have anything
    to reap so we end up with zombies. So in that case we'll pass the pid;
    we need to move this check to after the start of the process so that we
    have a valid pid.

--- commands/commands.go
+++ commands/commands.go
@@ -91,7 +91,17 @@ func RunAndWait(c *Command) (int, error) {
 		fmt.Sprintf("CONTAINERPILOT_%s_PID", strings.ToUpper(c.Name)),
 		fmt.Sprintf("%v", c.Cmd.Process.Pid),
 	)
-	defer reapChildren(c.Cmd.SysProcAttr.Pgid)
+	// See https://golang.org/src/syscall/exec_linux.go
+	// SysProcAttr.Setpgid:
+	// "Set process group ID to Pgid, or, if Pgid == 0, to new pid"
+	// So in the case where ContainerPilot is PID1 this will fail
+	// to reap zombies unless we pass the PID and not the PGID to
+	// the syscall.Wait4 in reapChildren
+	pgid := c.Cmd.SysProcAttr.Pgid
+	if pgid == 0 {
+		pgid = c.Cmd.Process.Pid
+	}
+	defer reapChildren(pgid)
 	err := c.Cmd.Wait()
 	if err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
@@ -125,7 +135,18 @@ func RunAndWaitForOutput(c *Command) (string, error) {
 	// to write to our collector.
 	c.Cmd.Stderr = os.Stderr
 	log.Debugf("%s.Cmd.Output", c.Name)
-	defer reapChildren(c.Cmd.SysProcAttr.Pgid)
+
+	// See https://golang.org/src/syscall/exec_linux.go
+	// SysProcAttr.Setpgid:
+	// "Set process group ID to Pgid, or, if Pgid == 0, to new pid"
+	// So in the case where ContainerPilot is PID1 this will fail
+	// to reap zombies unless we pass the PID and not the PGID to
+	// the syscall.Wait4 in reapChildren
+	pgid := c.Cmd.SysProcAttr.Pgid
+	if pgid == 0 {
+		pgid = c.Cmd.Process.Pid
+	}
+	defer reapChildren(pgid)
 	out, err := c.Cmd.Output()
 	if err != nil {
 		return "", err
@@ -209,7 +230,18 @@ func (c *Command) waitForTimeout() error {
 	}
 	log.Debugf("%s.run waiting", c.Name)
 
-	defer reapChildren(c.Cmd.SysProcAttr.Pgid)
+	// See https://golang.org/src/syscall/exec_linux.go
+	// SysProcAttr.Setpgid:
+	// "Set process group ID to Pgid, or, if Pgid == 0, to new pid"
+	// So in the case where ContainerPilot is PID1 this will fail
+	// to reap zombies unless we pass the PID and not the PGID to
+	// the syscall.Wait4 in reapChildren
+	pgid := c.Cmd.SysProcAttr.Pgid
+	if pgid == 0 {
+		pgid = c.Cmd.Process.Pid
+	}
+	defer reapChildren(pgid)
+
 	err := c.Cmd.Wait()
 	if err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
