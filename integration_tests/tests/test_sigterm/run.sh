a80bac8e267f7bfe78dc1d07af7ac9cb7858fd98
Tim Gross <tim@0x74696d.com>
2017-06-30 10:56:02 -0400

    remove commented-out sections of integration test

--- integration_tests/tests/test_sigterm/run.sh
+++ integration_tests/tests/test_sigterm/run.sh
@@ -5,15 +5,8 @@ set -e
 function finish {
     local result=$?
     if [ $result -ne 0 ]; then
-        CONSUL_ID="$(docker-compose ps -q consul)"
-        TEST_ID=$(docker ps -l -f "ancestor=cpfix_test_probe" --format="{{.ID}}")
-
-        echo '----- TEST LOGS ------'
-        #docker logs "$TEST_ID" | tee test.log
-        #echo '----- APP LOGS ------'
+        echo '----- APP LOGS ------'
         docker logs "$APP_ID" | tee app.log
-        #echo '----- CONSUL LOGS ------'
-        #docker logs "$CONSUL_ID" | tee consul.log
         echo '---------------------'
     fi
     exit $result
