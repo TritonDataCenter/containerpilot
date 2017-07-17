883b4676a31421d89d8c2b4ea8564592638c6bd3
Tim Gross <tim@0x74696d.com>
2017-07-17 16:29:52 -0400

    clean up Command.Run to eliminate unused exit err handling
    Note that we can't eliminate the bulk of this section by using stdlib
    CommandContext, because the child processes may have side-effects.
    Instead we assign a unique process group ID and then kill all the
    child processes when the context is done.

--- commands/commands.go
+++ commands/commands.go
@@ -53,34 +53,26 @@ func (c *Command) Run(pctx context.Context, bus *events.EventBus) {
 	// realistic configuration but this ensures that's the case
 	c.lock.Lock()
 	log.Debugf("%s.Run start", c.Name)
-	c.setUpCmd()
-	c.Cmd.Stdout = c.logger.Writer()
-	c.Cmd.Stderr = c.logger.Writer()
 
-	var (
-		ctx    context.Context
-		cancel context.CancelFunc
-	)
-	if c.Timeout > 0 {
-		ctx, cancel = context.WithTimeout(pctx, c.Timeout)
-	} else {
-		ctx, cancel = context.WithCancel(pctx)
-	}
+	cmd := ArgsToCmd(c.Exec, c.Args)
+	cmd.Stdout = c.logger.Writer()
+	cmd.Stderr = c.logger.Writer()
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	c.Cmd = cmd
+	ctx, cancel := getContext(pctx, c.Timeout)
 
 	go func() {
-		select {
-		case <-ctx.Done():
-			// unlock only here because we'll receive this
-			// cancel from both a typical exit and a timeout
-			defer c.lock.Unlock()
-			if ctx.Err() == context.DeadlineExceeded {
-				log.Warnf("%s timeout after %s: '%s'", c.Name, c.Timeout, c.Args)
-			}
-			// if the context was canceled we don't know if its because we
-			// canceled it in the caller or the application exited gracefully,
-			// so Kill() will have to handle both cases safely
-			c.Kill()
+		// Children may have side-effects so we don't want to wait for them
+		// to be reaped if we timeout, so we can't use CommandContext from
+		// the stdlib. Instead we've assigned a unique process group ID, so
+		// here we'll block until the context is done and then unlock and kill
+		// all child processes.
+		<-ctx.Done()
+		defer c.lock.Unlock()
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Warnf("%s timeout after %s: '%s'", c.Name, c.Timeout, c.Args)
 		}
+		c.Kill()
 	}()
 
 	go func() {
@@ -93,11 +85,12 @@ func (c *Command) Run(pctx context.Context, bus *events.EventBus) {
 			return
 		}
 		// blocks this goroutine here; if the context gets cancelled
-		// we'll return from wait() and do all the cleanup
-		if err := c.wait(); err != nil {
+		// we'll return from Wait() and publish events
+		if err := c.Cmd.Wait(); err != nil {
 			log.Errorf("%s exited with error: %v", c.Name, err)
 			bus.Publish(events.Event{events.ExitFailed, c.Name})
-			bus.Publish(events.Event{events.Error, err.Error()})
+			bus.Publish(events.Event{events.Error,
+				fmt.Errorf("%s: %s", c.Name, err).Error()})
 		} else {
 			log.Debugf("%s exited without error", c.Name)
 			bus.Publish(events.Event{events.ExitSuccess, c.Name})
@@ -105,31 +98,15 @@ func (c *Command) Run(pctx context.Context, bus *events.EventBus) {
 	}()
 }
 
-func (c *Command) wait() error {
-	err := c.Cmd.Wait()
-	if err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
-				if status.ExitStatus() == 0 {
-					return nil
-				}
-			}
-		}
-		return fmt.Errorf("%s: %s", c.Name, err.Error())
+func getContext(pctx context.Context, timeout time.Duration) (context.Context, context.CancelFunc) {
+	if timeout > 0 {
+		return context.WithTimeout(pctx, timeout)
 	}
-	return nil
+	return context.WithCancel(pctx)
 }
 
-func (c *Command) setUpCmd() {
-	cmd := ArgsToCmd(c.Exec, c.Args)
-
-	// assign a unique process group ID so we can kill all
-	// its children on timeout
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	c.Cmd = cmd
-}
-
-// Kill sends a kill signal to the underlying process, if it still exists
+// Kill sends a kill signal to the underlying process if it still exists,
+// as well as all its children
 func (c *Command) Kill() {
 	log.Debugf("%s.kill", c.Name)
 	if c.Cmd != nil && c.Cmd.Process != nil {
