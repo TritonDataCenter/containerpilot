55749e7c1c1569eb4dc00703078aafe90d9d9a1a
Tim Gross <tim@0x74696d.com>
2017-07-28 09:49:10 -0400

    update test_telemetry to catch /status regressions

---
+++ integration_tests/tests/test_telemetry/check.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eo pipefail
+
+IP="$1"
+curl -s "${IP}:9090/status" | json -a .Services.0.Status | grep "healthy"
+curl -s "${IP}:9090/status" | json -a .Services.1.Status | grep "healthy"
