c994266284c3eaf4c540d2059aa455c216103e57
Tim Gross <tim@0x74696d.com>
2017-07-28 16:36:43 -0400

    add more context to test_coprocess failures

--- integration_tests/tests/test_coprocess/run.sh
+++ integration_tests/tests/test_coprocess/run.sh
@@ -21,29 +21,33 @@ if [ ! $? -eq 0 ] ; then exit 1 ; fi
 ID=$(docker ps -l -f "ancestor=cpfix2_app" --format="{{.ID}}")
 
 # verify the coprocess is running
-docker exec -it "$ID" ps -ef | grep coprocess
+docker exec -it "$ID" ps -ef | grep coprocess \
+    || ( echo 'coprocess not running' && exit 1 )
 
 # kill the coprocess and verify it restarts
 docker exec -it "$ID" pkill coprocess
 sleep 1
-docker exec -it "$ID" ps -ef | grep coprocess
+docker exec -it "$ID" ps -ef | grep coprocess \
+    || ( echo 'coprocess not restarted' && exit 1 )
 
 # kill the coprocess and verify it doesn't restart again
 docker exec -it "$ID" pkill coprocess
 sleep 1
-
-set +e
-docker exec -it "$ID" ps -ef | grep coprocess && exit 1
-set +e
+docker exec -it "$ID" ps -ef | grep coprocess \
+    && ( echo 'coprocess should be stopped' && exit 1 )
 
 # update the ContainerPilot config and verify the coprocess is running
 # with the new flags (this resets the restart limit)
 docker exec -it "$ID" sed -i 's/arg1/arg2/' /app-with-coprocess.json
 docker exec -it "$ID" kill -SIGHUP 1
 sleep 1
-docker exec -it "$ID" ps -ef | grep coprocess | grep arg2
+docker exec -it "$ID" ps -ef | grep coprocess | grep arg2 \
+    || ( echo 'coprocess should use new args, got:' \
+              && docker exec -it "$ID" ps -ef \
+              && exit 1 )
 
 # kill the coprocess and verify it restarts
 docker exec -it "$ID" pkill coprocess
 sleep 1
-docker exec -it "$ID" ps -ef | grep coprocess
+docker exec -it "$ID" ps -ef | grep coprocess \
+    || ( echo 'coprocess not restarted w/ new args' && exit 1 )
