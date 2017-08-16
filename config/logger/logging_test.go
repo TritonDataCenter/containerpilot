45b95b6e85da098512d4a9797273d820cd959233
Benoit Toulme <benoit.toulme@gmail.com>
2017-08-16 11:53:49 -0700

    test for file logger

--- config/logger/logging_test.go
+++ config/logger/logging_test.go
@@ -1,9 +1,14 @@
 package logger
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"reflect"
+	"strings"
+	"syscall"
 	"testing"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -64,4 +69,67 @@ func TestDefaultFormatterPanic(t *testing.T) {
 		}
 	}()
 	logrus.Panicln("Panic Test")
+}
+
+func TestFileLogger(t *testing.T) {
+	// initialize logger
+	filename := "/tmp/test_log"
+	testLog := &Config{
+		Level:  "DEBUG",
+		Format: "text",
+		Output: filename,
+	}
+	err := testLog.Init()
+	if err != nil {
+		t.Errorf("Did not expect error: %v", err)
+	}
+
+	testContainsLog := func(fname string, log string, t *testing.T) {
+		content, err := ioutil.ReadFile(fname)
+		if err != nil {
+			t.Errorf("Did not expect error: %v", err)
+		}
+		if len(content) == 0 {
+			t.Error("could not write log to file")
+		}
+		logs := string(content)
+		if !strings.Contains(logs, log) {
+			t.Errorf("expected log file to contain '%s', got '%s'", log, logs)
+		}
+	}
+
+	// write a log message
+	logMsg := "this is a test"
+	logrus.Info(logMsg)
+	testContainsLog(filename, logMsg, t)
+
+	// rotate the log
+	rotatedFilename := fmt.Sprintf("%s.1", filename)
+	err = os.Rename(filename, rotatedFilename)
+	if err != nil {
+		t.Errorf("Did not expect error: %v", err)
+	}
+	logMsg = "this is another test"
+	logrus.Info(logMsg)
+	testContainsLog(rotatedFilename, logMsg, t)
+
+	// reopen file
+	syscall.Kill(os.Getpid(), syscall.SIGUSR1)
+	time.Sleep(1*time.Second)
+
+	timeExpire := time.Now().Add(3 * time.Second)
+	for {
+		_, err = os.Stat(filename)
+		if err == nil {
+			break
+		}
+		if timeExpire.After(time.Now()) {
+			t.Error("Did not reopen file in expected time")
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+	logMsg = "this is the last test"
+	logrus.Info(logMsg)
+	testContainsLog(filename, logMsg, t)
 }
