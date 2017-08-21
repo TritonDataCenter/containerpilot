5e88da277d56154c5017b95b9daa152614ce20b1
Tim Gross <tim@0x74696d.com>
2017-08-21 09:30:10 -0400

    move defer out of loop so as not to leak a closure (#488)
    * move defer out of loop so as not to leak a closure
    * move handleSignals out of run loop because we never cancel it

--- events/timer.go
+++ events/timer.go
@@ -41,18 +41,18 @@ func NewEventTimer(
 ) {
 	go func() {
 		ticker := time.NewTicker(tick)
+		// sending the timeout event potentially races with a closing
+		// rx channel, so just recover from the panic and exit
+		defer func() {
+			if r := recover(); r != nil {
+				return
+			}
+		}()
 		for {
 			select {
 			case <-ctx.Done():
 				return
 			case <-ticker.C:
-				// sending the timeout event potentially races with a closing
-				// rx channel, so just recover from the panic and exit
-				defer func() {
-					if r := recover(); r != nil {
-						return
-					}
-				}()
 				rx <- Event{Code: TimerExpired, Source: name}
 			}
 		}
