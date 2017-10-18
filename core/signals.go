bd75e618d5c29372acf52efe1e643c5eee938b3f
Justin Reagor <justin.reagor@joyent.com>
2017-10-18 15:33:52 -0400

    Fix a goroutine leak in the signal handler code path
    The root cause of this issue was regarding our change to shutting down via
    context. While implementating that change set there was a goroutine leak
    introduced that did not provide a clean way for shutting down and reloading the
    signal handler. This caused signal handling to be duplicated across reloads.
    
    Fixes: #521

--- core/signals.go
+++ core/signals.go
@@ -8,20 +8,26 @@ import (
 )
 
 // HandleSignals listens for and captures signals used for orchestration
-func (a *App) handleSignals(cancel context.CancelFunc) {
+func (a *App) handleSignals(ctx context.Context, cancel context.CancelFunc) {
 	recvSig := make(chan os.Signal, 1)
 	signal.Notify(recvSig, syscall.SIGTERM, syscall.SIGINT)
 	go func() {
-		for sig := range recvSig {
-			switch sig {
-			case syscall.SIGINT:
-				a.Terminate()
-				break
-			case syscall.SIGTERM:
-				a.Terminate()
-				break
+		for {
+			select {
+			case sig := <-recvSig:
+				switch sig {
+				case syscall.SIGINT:
+					a.Terminate()
+					return
+				case syscall.SIGTERM:
+					a.Terminate()
+					return
+				default:
+				}
+			case <-ctx.Done():
+				return
+			default:
 			}
 		}
-		cancel()
 	}()
 }
