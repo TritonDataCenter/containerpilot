2c245e6ce8a7cb8b05598a18517791560b988d2e
teutates <10206665+teutat3s@users.noreply.github.com>
2020-02-03 20:28:37 +0100

    Add consul checksum for download verification
    More integration test fixes
    
    Bump go version 1.14
    
    Bump consul version to 1.7.3
    
    Fix coprocess integration test
    
    Fix integration tests with drone CI

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,19 @@
-FROM golang:1.13
+FROM golang:1.14
 
-ENV CONSUL_VERSION=1.6.3
+ENV CONSUL_VERSION=1.7.3
 
 RUN  apt-get update \
      && apt-get install -y unzip \
-     && go get golang.org/x/lint/golint \
-     && curl --fail -Lso consul.zip "https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip" \
-     && unzip consul.zip -d /usr/bin
+     && go get golang.org/x/lint/golint
+
+RUN export CONSUL_CHECKSUM=453814aa5d0c2bc1f8843b7985f2a101976433db3e6c0c81782a3c21dd3f9ac3 \
+    && export archive=consul_${CONSUL_VERSION}_linux_amd64.zip \
+    && curl -Lso /tmp/${archive} https://releases.hashicorp.com/consul/${CONSUL_VERSION}/${archive} \
+    && echo "${CONSUL_CHECKSUM}  /tmp/${archive}" | sha256sum -c \
+    && cd /bin \
+    && unzip /tmp/${archive} \
+    && chmod +x /bin/consul \
+    && rm /tmp/${archive}
 
 ENV CGO_ENABLED 0
 ENV GOPATH /go:/cp
--- integration_tests/tests/test_telemetry/run.sh
+++ integration_tests/tests/test_telemetry/run.sh
@@ -5,19 +5,18 @@ set -e
 # start up consul and wait for leader election
 docker-compose up -d consul
 consul=$(docker-compose ps -q consul)
-docker exec -it "$consul" assert ready
+docker exec -i "$consul" assert ready
 
 # start up app
 docker-compose up -d app
 
 app="$(docker-compose ps -q app)"
-IP=$(docker inspect -f '{{ .NetworkSettings.Networks.testtelemetry_default.IPAddress }}' "$app")
-docker exec -it "$app" /bin/containerpilot -putmetric 'containerpilot_app_some_other_counter=42'
+IP=$(docker inspect -f '{{ .NetworkSettings.Networks.test_telemetry_default.IPAddress }}' "$app")
 
 # This interface takes a while to converge
 for _ in $(seq 0 20); do
     sleep 1
-    metrics=$(docker exec -it "$app" curl -s "${IP}:9090/metrics")
+    metrics=$(docker exec -i "$app" curl -s "${IP}:9090/metrics")
     echo "$metrics" | grep 'containerpilot_app_some_counter 42' && break
 done || (echo "did not get expected metrics output" && exit 1)
 
@@ -30,9 +29,9 @@ echo "$metrics" | grep 'containerpilot_watch_instances' || \
     ( echo 'no containerpilot_watch_instances metrics' && exit 1 )
 
 # Check the status endpoint too
-docker exec -it "$app" /check.sh "${IP}"
+docker exec -i "$app" /check.sh "${IP}"
 
 # Make we register and tear down telemetry service in Consul
-docker exec -it "$consul" assert service containerpilot 1
+docker exec -i "$consul" assert service containerpilot 1
 docker-compose stop app
-docker exec -it "$consul" curl -s --fail localhost:8500/v1/catalog/service/containerpilot | grep -v 'containerpilot'
+docker exec -i "$consul" curl -s --fail localhost:8500/v1/catalog/service/containerpilot | grep -v 'containerpilot'
