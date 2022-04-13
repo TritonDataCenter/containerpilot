7acbd4d30374858dd889466fab9868643f0d0506
teutat3s <10206665+teutat3s@users.noreply.github.com>
2022-04-13 16:41:09 +0200

    Fix lint target for go modules

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 result=0
-for pkg in $(glide novendor)
+for pkg in $(go list ./...)
 do
     golint -set_exit_status "$pkg" || result=1
     go vet "$pkg" || result=1
